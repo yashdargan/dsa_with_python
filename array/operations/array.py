1d2cef7d066092787bbb9a972b4331f4e9791df5
yashdargan <yashrajdargan@gmail.com>
2024-08-08 13:17:34 +0530

    deletion in array

--- array/operations/array.py
+++ array/operations/array.py
@@ -3,7 +3,7 @@ def traverse(arr: list):
 
 
 def insertion(arr: list, num: int, pos: int):
-    # insertion at starting IndexError
+    # insertion check
     if pos < 0 or pos > len(arr)-1:
         print('invalid postion')
         return
@@ -16,17 +16,29 @@ def insertion(arr: list, num: int, pos: int):
         traverse(arr)
 
 
+def deletion(arr: list, pos: int):
+    if pos < 0 or pos > len(arr)-1:
+        print("invalid postion")
+        return
+    else:
+        for i in range(pos, len(arr)-1):
+            arr[i] = arr[i+1]
+        arr.pop(len(arr)-1)
+        traverse(arr)
+
+
 if __name__ == "__main__":
     n = int(input("enter the number\n"))
     arr = []
     for i in range(0, n):
         value = int(input(f"enter the value {i+1} "))
         arr.append(value)
-    dis = int(input("want to do insertion -> 0 or traversal -> 1 "))
+    dis = int(input("want to do insertion -> 0 or deletion -> 1 "))
     if dis == 0:
         pos = int(input("enter the position "))
         num = int(input("enter the number "))
 
         insertion(arr, num, pos)
     elif dis == 1:
-        traverse(arr)
+        pos = int(input("enter the position "))
+        deletion(arr, pos)
