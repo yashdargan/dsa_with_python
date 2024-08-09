3497a8a7f2f345f406ad53b1f7f558b300cb6641
yashdargan <yashrajdargan@gmail.com>
2024-08-09 15:18:44 +0530

    inseration & deletion At middle

--- list/operations/list.py
+++ list/operations/list.py
@@ -29,6 +29,40 @@ class Linklist:
             new_node.next = self.head
             self.head = new_node
 
+    def inserationAtMiddle(self, new_value):
+        new_node = Node(new_value)
+        if new_node:
+            middle = self.head
+            curr = self.head.next
+            while curr.next and curr:
+                middle = middle.next
+                curr = curr.next.next
+            after = middle.next
+            middle.next = new_node
+            new_node.next = after
+
+    def deletionATStart(self):
+        ptr = self.head.next
+        self.head = ptr
+
+    def deletionAtEnd(self):
+        ptr = self.head
+        utr = None
+        while ptr.next:
+            utr = ptr
+            ptr = ptr.next
+        utr.next = None
+
+    def deletionAtMiddle(self):
+        ptr = self.head.next
+        utr = None
+        middle = self.head
+        while ptr and ptr.next:
+            utr = middle
+            middle = middle.next
+            ptr = ptr.next.next
+        utr.next = middle.next
+        middle.next = None
         # printing the value of Linklist
 
     def Printing(self):
@@ -46,4 +80,12 @@ if __name__ == "__main__":
     ll.append("dog")
     ll.Printing()
     ll.inserationAtStart("cat")
+    ll.Printing()
+    ll.deletionATStart()
+    ll.Printing()
+    ll.deletionAtEnd()
+    ll.Printing()
+    ll.inserationAtMiddle(4)
+    ll.Printing()
+    ll.deletionAtMiddle()
     ll.Printing()
